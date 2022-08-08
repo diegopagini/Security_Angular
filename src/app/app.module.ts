0679851e543c4bf50278a99ba637c07d17532808
Diego Pagini <diegopaginidev@gmail.com>
2022-08-08 10:20:37 +0200

    prettier

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,30 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import {HttpClientModule} from '@angular/common/http';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
 
-import { AppComponent } from './app.component';
-import { LessonsComponent } from './lessons/lessons.component';
-import { LoginComponent } from './login/login.component';
-import { SignupComponent } from './signup/signup.component';
-import {RouterModule} from "@angular/router";
-import {routesConfig} from "./routes.config";
-import {LessonsService} from "./services/lessons.service";
-import {ReactiveFormsModule} from "@angular/forms";
-
-
+import { AppComponent } from "./app.component";
+import { LessonsComponent } from "./lessons/lessons.component";
+import { LoginComponent } from "./login/login.component";
+import { SignupComponent } from "./signup/signup.component";
+import { RouterModule } from "@angular/router";
+import { routesConfig } from "./routes.config";
+import { LessonsService } from "./services/lessons.service";
+import { ReactiveFormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [
     AppComponent,
     LessonsComponent,
     LoginComponent,
-    SignupComponent
+    SignupComponent,
   ],
   imports: [
     BrowserModule,
-      HttpClientModule,
-      RouterModule.forRoot(routesConfig),
-      ReactiveFormsModule
+    HttpClientModule,
+    RouterModule.forRoot(routesConfig),
+    ReactiveFormsModule,
   ],
   providers: [LessonsService],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule {
-
-}
+export class AppModule {}
